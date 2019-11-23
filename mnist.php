08317ce05d1944d641be2c0f20b6a7a505ab0835
luoxiaojun <luoxiaojun@dodoca.com>
2019-11-23 16:34:31 +0800

    update

--- mnist.php
+++ mnist.php
@@ -19,6 +19,8 @@ $testDataset = new \Phpml\Dataset\CsvDataset(
 );
 $testSamples = $testDataset->getSamples();
 $testTargets = $testDataset->getTargets();
+$usage = microtime(true) - $start;
+echo 'Finished at: ' . ((string)$usage) . 's', PHP_EOL;
 
 echo 'Processing samples...', PHP_EOL;
 foreach ($trainSamples as $i => $row) {
@@ -37,16 +39,24 @@ foreach ($testSamples as $i => $row) {
 foreach ($testTargets as $i => $target) {
     $testTargets[$i] = intval($target);
 }
+$usage = microtime(true) - $start;
+echo 'Finished at: ' . ((string)$usage) . 's', PHP_EOL;
 
 echo 'Network init...', PHP_EOL;
 $classifier = new \Phpml\Classification\SVC();
+$usage = microtime(true) - $start;
+echo 'Finished at: ' . ((string)$usage) . 's', PHP_EOL;
 
 echo 'Training...', PHP_EOL;
 $classifier->train($trainSamples, $trainTargets);
+$usage = microtime(true) - $start;
+echo 'Finished at: ' . ((string)$usage) . 's', PHP_EOL;
 
 echo 'Evaluating...', PHP_EOL;
 $predicted = $classifier->predict($testSamples);
 echo 'Score: ' . ((string)(\Phpml\Metric\Accuracy::score($testTargets, $predicted))), PHP_EOL;
+$usage = microtime(true) - $start;
+echo 'Finished at: ' . ((string)$usage) . 's', PHP_EOL;
 
 echo 'Saving model...', PHP_EOL;
 $modelsDir = __DIR__ . '/models/';
@@ -54,6 +64,5 @@ if (!is_dir($modelsDir)) {
     mkdir($modelsDir, 0777, true);
 }
 (new \Phpml\ModelManager())->saveToFile($classifier, $modelsDir . 'mnist.model');
-
 $usage = microtime(true) - $start;
-echo 'Usage: ' . ((string)$usage) . 's', PHP_EOL;
+echo 'Finished at: ' . ((string)$usage) . 's', PHP_EOL;
